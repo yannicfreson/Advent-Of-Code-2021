f931312840099039b5a7ccd2339b57de3badddc7
Storm <61149527+yannicfreson@users.noreply.github.com>
2021-12-01 16:06:52 +0100

    upd

---
+++ 1_Sonar_Sweep/part_one.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input;
+let larger = 0;
+fs.readFile("input.txt", "utf8", function (err, data) {
+  input = data.split("\n");
+  for (let i = 0; i < input.length - 1; i++) {
+    if (parseInt(input[i]) < parseInt(input[i + 1])) {
+      larger++;
+    }
+  }
+  console.log(larger);
+});
